85d1a77cd6ca88eecb376c039bd616431b26249c
TheDeveloper10 <th3r3ap3r@abv.bg>
2023-02-07 17:12:31 +0200

    Added more integer functionality

--- intValidator.go
+++ intValidator.go
@@ -1,10 +1,25 @@
 package errorland
 
 import (
-	"errors"
 	"fmt"
 	"strconv"
 )
+
+func IntMin(in int, name string, min int) error {
+	if in < min {
+		return fmt.Errorf(IntMinErrMsg, name, min)
+	}
+
+	return nil
+}
+
+func IntMax(in int, name string, min int) error {
+	if in > min {
+		return fmt.Errorf(IntMaxErrMsg, name, min)
+	}
+
+	return nil
+}
 
 func IntMinMax(in int, name string, min int, max int) error {
 	if in < min {
